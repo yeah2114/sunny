3887062c891ba34f1a5931b3f65fe6f45a9a377d
yeahseul <yeah2114@naver.com>
2020-10-21 18:06:31 +0900

    up

---
+++ js/index.js
@@ -0,0 +1,31 @@
+$(function(){
+    $('.ban figure').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+    
+    $('.slick-next').hide();
+    $('.slick-prev').hide();
+    
+    
+//    var mq = window.matchMedia("screen and (max-width:480px)");
+//
+//    mq.addListener(res);
+//
+//    function res(e) {
+//        if (e.matches) {
+//            $('.ban1').attr('src', 'img/ban1.png');
+//
+//            $('.ban2').attr('src', 'img/ban2.png');
+//           
+//            var txt = $('.review figcaption').eq(0).text();
+//                    
+//        } else {
+//            console.log('PC');
+//        }
+//    }
+});
